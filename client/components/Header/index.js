6e83b520bda4d3c20b1e223e7849596cef9ae492
jorgehg <j.herrando.dev@gmail.com>
2022-06-23 14:41:20 +0200

    header design

--- client/components/Header/index.js
+++ client/components/Header/index.js
@@ -1,10 +1,40 @@
+import Button from "react-bootstrap/Button";
 import headerStyles from "./Header.module.css";
 
 export default function Header({ logged, handleLogout }) {
   return (
-    <div className={headerStyles.headerContainer}>
-      <p>Este el el header</p>
-      {logged && <button onClick={handleLogout}>Logout</button>}
-    </div>
+    <header className={headerStyles.headerContainer}>
+      <div className={headerStyles.innerHeader}>
+        {logged && (
+          <div className={headerStyles.logoutContainer}>
+            <div className={headerStyles.logoutArrow}>
+              <a onClick={handleLogout}>
+                <img src="/img/icons/arrowLeft.png" alt="logout" />
+              </a>
+            </div>
+            <Button
+              variant="secondary"
+              onClick={handleLogout}
+              className={headerStyles.logoutButton}
+            >
+              {" "}
+              Logout
+            </Button>
+          </div>
+        )}
+
+        <div className={headerStyles.titleContainer}>
+          <div className={headerStyles.dogeCoinIcon}>
+            <img src="/img/icons/dogecoin.png" alt="dogecoin" />
+          </div>
+          <h1>DogeCoin</h1>
+        </div>
+        <div className={headerStyles.settingsIconContainer}>
+          <div className={headerStyles.settingsIcon}>
+            <img src="/img/icons/settingsIcon.png" alt="settings" />
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
