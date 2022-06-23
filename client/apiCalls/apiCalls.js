1b9522d7c789a34c4e04002d9d7ac2857bb57000
jorgehg <j.herrando.dev@gmail.com>
2022-06-23 22:40:02 +0200

    trying .env

--- client/apiCalls/apiCalls.js
+++ client/apiCalls/apiCalls.js
@@ -2,37 +2,46 @@
 const apiCalls = {
   // log the user in
   login: async (email, password) => {
-    const call = await fetch(`http://localhost:3001/api/users/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
+    const call = await fetch(
+      `${process.env.NEXT_PUBLIC_URL_BACK}/users/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }
+    );
     const data = await call.json();
     return data;
   },
   // get user data
   getUser: async (id) => {
-    const call = await fetch(`http://localhost:3001/api/users/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const call = await fetch(
+      `${process.env.NEXT_PUBLIC_URL_BACK}/users/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     const data = await call.json();
     return data;
   },
   // add money to wallet
   addMoney: async (amount, wallet, token) => {
-    const call = await fetch(`http://localhost:3001/api/transactions/add`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ amount, address: wallet }),
-    });
+    const call = await fetch(
+      `${process.env.NEXT_PUBLIC_URL_BACK}/transactions/add`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ amount, address: wallet }),
+      }
+    );
     const data = await call.json();
     return data;
   },
